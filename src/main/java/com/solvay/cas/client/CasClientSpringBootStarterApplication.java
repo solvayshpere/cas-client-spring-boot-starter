aa9c32748205c83807d0eb77624b2e0b41af132e
ipixel <ipixel@kangxin.com>
2020-08-24 19:08:50 +0800

    单点登录 自定义Starter

---
+++ src/main/java/com/solvay/cas/client/CasClientSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.solvay.cas.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasClientSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasClientSpringBootStarterApplication.class, args);
+    }
+
+}
